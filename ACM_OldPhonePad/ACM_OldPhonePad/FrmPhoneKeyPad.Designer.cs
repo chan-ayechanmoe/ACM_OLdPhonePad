56449a5e565c856f34fbf31e795d63af69b46401
Chan-acm <ayechanmoe.cm@gmail.com>
2024-09-19 12:34:55 +0700

    file upload

---
+++ ACM_OldPhonePad/ACM_OldPhonePad/FrmPhoneKeyPad.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ACM_OldPhonePad
+{
+    partial class FrmPhoneKeyPad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtPhKey = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnShowAns = new System.Windows.Forms.Button();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtPhKey
+            // 
+            this.txtPhKey.Location = new System.Drawing.Point(12, 99);
+            this.txtPhKey.Multiline = true;
+            this.txtPhKey.Name = "txtPhKey";
+            this.txtPhKey.Size = new System.Drawing.Size(707, 108);
+            this.txtPhKey.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(206, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(303, 37);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Old Phone KeyPad";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter phone\'s key :";
+            // 
+            // btnShowAns
+            // 
+            this.btnShowAns.Location = new System.Drawing.Point(282, 227);
+            this.btnShowAns.Name = "btnShowAns";
+            this.btnShowAns.Size = new System.Drawing.Size(138, 48);
+            this.btnShowAns.TabIndex = 5;
+            this.btnShowAns.Text = "Show Answer";
+            this.btnShowAns.UseVisualStyleBackColor = true;
+            this.btnShowAns.Click += new System.EventHandler(this.btnShowAns_Click);
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(12, 300);
+            this.txtAnswer.Multiline = true;
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.ReadOnly = true;
+            this.txtAnswer.Size = new System.Drawing.Size(707, 108);
+            this.txtAnswer.TabIndex = 6;
+            // 
+            // FrmPhoneKeyPad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 450);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.btnShowAns);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPhKey);
+            this.Name = "FrmPhoneKeyPad";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Old Phone KeyPad";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPhKey;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnShowAns;
+        private System.Windows.Forms.TextBox txtAnswer;
+    }
+}
---
+++ ACM_OldPhonePad/ACM_OldPhonePad/FrmPhoneKeyPad.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ACM_OldPhonePad
+{
+    public partial class FrmPhoneKeyPad : Form
+    {
+        public FrmPhoneKeyPad()
+        {
+            InitializeComponent();
+        }
+        #region "Event"
+        private void btnShowAns_Click(object sender, EventArgs e)
+        {
+            string inputKey = "";
+            string result = "";
+            if (!String.IsNullOrEmpty(txtPhKey.Text))
+            {
+                //Assumes “#” keyword will always be included at the end of every input.
+                //if not include, add "#" at the end of input.
+                inputKey = txtPhKey.Text.Contains('#') ? txtPhKey.Text.ToString().Trim() : txtPhKey.Text.ToString().Trim() + "#";
+                result = OldPhonePad(inputKey);
+                if (!String.IsNullOrEmpty(result))
+                {
+                    txtAnswer.Text = result;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter phone's key", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+        }
+        #endregion
+
+        #region "Function"
+        public static string OldPhonePad(string userInput)
+        {
+            bool isErr = false;
+            string result = "";
+            char[] letterList;
+            char currentLetter = '\0';
+            int letterCount = 0;
+
+            //Dictionary is for phone's key and value pairs 
+            //This is data mapping for phone's digit and letter 
+            Dictionary<int, string> phkey_Dict = new Dictionary<int, string>();
+            phkey_Dict.Add('2', "ABC");
+            phkey_Dict.Add('3', "DEF");
+            phkey_Dict.Add('4', "GHI");
+            phkey_Dict.Add('5', "JKL");
+            phkey_Dict.Add('6', "MNO");
+            phkey_Dict.Add('7', "PQRS");
+            phkey_Dict.Add('8', "TUV");
+            phkey_Dict.Add('9', "WXYZ");
+
+            foreach (char key in userInput)
+            {
+                if (phkey_Dict.ContainsKey(key))
+                {
+                    //Check user input key is same or different
+                    //If input is the same, increase letter's count
+                    if (currentLetter == key)
+                    {
+                        letterCount++;
+                    }
+                    else
+                    {
+                        //if input is different,  ouput the result
+                        if (currentLetter != '\0' && letterCount > 0)
+                        {
+                            if (CheckKeyCount(currentLetter, letterCount))
+                            {
+                                letterList = phkey_Dict[currentLetter].ToCharArray();
+                                result = result + letterList[letterCount - 1].ToString();
+                            }
+                            else
+                            {
+                                isErr = true;
+                                MessageBox.Show("Invalid key. Please enter space for more than three same character.", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Information);  
+                            }  
+                        }
+                        if (isErr) break;
+                        else 
+                        //update the current letter and set the count to 1 
+                        currentLetter = key;
+                        letterCount = 1;
+                       
+
+                    }
+                }
+                else if (key == '#')
+                {
+                    //If key is '#', output the result
+                    if (currentLetter != '\0' && letterCount > 0)
+                    {
+                        if (CheckKeyCount(currentLetter, letterCount))
+                        {
+                            letterList = phkey_Dict[currentLetter].ToCharArray();
+                            result = result + letterList[letterCount - 1].ToString();
+                        }
+                        else
+                        {
+                            isErr = true;
+                            MessageBox.Show("Invalid key. Please enter space for more than three same character.", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                          
+                        }
+                    }
+                    if (isErr) break;
+                    else
+                    // set default character and count to 0
+                    currentLetter = '\0';
+                    letterCount = 0;
+                  
+                }
+                else if (key == '*')
+                {
+                    // '*' keyword is not to take the current letter and delete the previous letter
+                    // clear current letter and current count
+                    // reset default character and count to 0
+
+                    currentLetter = '\0';
+                    letterCount = 0;
+
+                }
+                else if (key == ' ')
+                {
+                    // ' ' keyword is not to take the current letter and ouput the result
+                    if (currentLetter != '\0' && letterCount > 0)
+                    {
+                        if (CheckKeyCount(currentLetter, letterCount))
+                        {
+                            letterList = phkey_Dict[currentLetter].ToCharArray();
+                            result = result + letterList[letterCount - 1].ToString();  
+                        }
+                        else
+                        {
+                            isErr = true;
+                            MessageBox.Show("Invalid key. Please enter space for more than three same character.", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+
+                    }
+                    if (isErr) break;
+                    else
+                    // set default character and count to 0
+                    currentLetter = '\0';
+                    letterCount = 0;
+                }
+            }
+
+            return result.ToString();
+
+        }
+        public static bool CheckKeyCount(char key, int count)
+        {
+            if (key == '7' || key == '9')
+            {
+                if (count > 4)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if(count>3)
+                {
+                    return false;
+                }
+            }
+               
+            return true;
+        }
+        #endregion
+    }
+}
